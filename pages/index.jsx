47dad70299ca340cfc66f7304d06c4de1df261aa
Andrey Emelianov <621077@mail.ru>
2024-02-19 20:16:07 +0300

    реализовал Ui компонент настраевомого модального окна, использовал createPortal для монитрования на страницу, добавил стилей

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,8 @@ import {
 } from "../components/game";
 import { Header } from "../components/header";
 import { GameSymbol } from "../components/game/game-symbol";
+import { UiModal } from "../components/uikit/ui-modal";
+import { UiButton } from "../components/uikit/ui-button";
 
 export default function HomePage() {
   const [playersCount] = useState(4);
@@ -40,6 +42,22 @@ export default function HomePage() {
             <GameSymbol symbol={winnerSymbol} />
           </div>
         )}
+        <UiModal width="md" isOpen={winnerSymbol}>
+          <UiModal.Header>Игра завершена!</UiModal.Header>
+          <UiModal.Body>
+            <div className="text-sm">
+              Победитель: <span className="text-teal-600">Emelianov</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size="md" variant="outline">
+              Вернуться
+            </UiButton>
+            <UiButton size="md" variant="primary">
+              Играть снова
+            </UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField
           className="mt-6"
           cells={cells}
